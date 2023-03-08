2f34b7cecc73317ddf41e95f6af5f358e6c1cc28
WHC <samuel.zurbriggen@gmail.com>
2023-03-08 11:44:05 +0100

    Updates Vereine (Lara) & Festprogramm

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function initMap() {
   });
 
   // Bar-marker
-  const bars = [
+  const drink_bars = [
     [
         "Bar Cipolla",
         46.311364,
@@ -16,57 +16,25 @@ function initMap() {
         "images/bar.png",
         25, 
         25,
-        '<h2>Bar Cipolla</h2><p><a href="">www.samisbest.com</a></p>'
+        '<h2>Bar Cipolla</h2>\
+        <h3 style="margin-block-end: 3px;">Öffnungszeiten</h3>\
+        <p style="margin-block-start: 3px;">Freitag: 18:00 Uhr - bis fertig<br>Samstag: 12:00 Uhr - bis fertig</p>\
+        <p>Preis: CHF 200.-</p>\
+        Status: Reserviert'
     ],
 
     [
-        "Garage Diego Zenhäusern",
+        "Garage Armando",
         46.311370,
         7.800530,
         "images/bar.png",
         25, 
         25,
-        '<h2>Garage Diego Zenhäusern</h2><p><a href="">www.samisbest.com</a></p>'
-    ],
-
-    [
-        "Parkplatz Maxenhaus",
-        46.311759,
-        7.800428,
-        "images/bar.png",
-        25, 
-        25,
-        '<h2>Parkplatz Maxenhaus</h2><p><a href="">www.samisbest.com</a></p>'
-    ],
-
-    [
-        "Burgersaal",
-        46.311560,
-        7.800238,
-        "images/bar.png",
-        25, 
-        25,
-        '<h2>Burgersaal</h2><p><a href="">www.samisbest.com</a></p>'
-    ],
-
-    [
-        "Stall vis-a-vis Burgersaal",
-        46.311605,
-        7.800008,
-        "images/bar.png",
-        25, 
-        25,
-        '<h2>Stall vis-a-vis Burgersaal</h2><p><a href="">www.samisbest.com</a></p>'
-    ],
-
-    [
-        "Platz vor Jublagarage",
-        46.311493,
-        7.800209,
-        "images/bar.png",
-        25, 
-        25,
-        '<h2>Platz vor Jublagarage</h2><p><a href="">www.samisbest.com</a></p>'
+        '<h2>Garage Armando</h2>\
+        <h3 style="margin-block-end: 3px;">Öffnungszeiten</h3>\
+        <p style="margin-block-start: 3px;">Freitag: 18:00 Uhr - bis fertig<br>Samstag: 18:00 Uhr - bis fertig</p>\
+        <p>Preis: CHF 200.-</p>\
+        Status: Frei'
     ],
 
     [
@@ -76,7 +44,11 @@ function initMap() {
         "images/bar.png",
         25, 
         25,
-        '<h2>Gemeinde Partylokal</h2><p><a href="">www.samisbest.com</a></p>'
+        '<h2>Gemeinde Partylokal</h2>\
+        <h3 style="margin-block-end: 3px;">Öffnungszeiten</h3>\
+        <p style="margin-block-start: 3px;">Freitag: 18:00 Uhr - bis fertig<br>Samstag: 18:00 Uhr - bis fertig</p>\
+        <p>Preis: CHF 300.-</p>\
+        Status: Frei'
     ],
 
     [
@@ -86,7 +58,11 @@ function initMap() {
         "images/bar.png",
         25, 
         25,
-        '<h2>Parking Gemeinde</h2><p><a href="">www.samisbest.com</a></p>'
+        '<h2>Parking Gemeinde</h2>\
+        <h3 style="margin-block-end: 3px;">Öffnungszeiten</h3>\
+        <p style="margin-block-start: 3px;">Freitag: 18:00 Uhr - bis fertig<br>Samstag: 11:00 Uhr - bis fertig</p>\
+        <p>Preis: CHF 300.-</p>\
+        Status: Frei'
     ],
 
     [
@@ -96,7 +72,11 @@ function initMap() {
         "images/bar.png",
         25, 
         25,
-        '<h2>Garage Chez Karlen</h2><p><a href="">www.samisbest.com</a></p>'
+        '<h2>Garage Chez Karlen</h2>\
+        <h3 style="margin-block-end: 3px;">Öffnungszeiten</h3>\
+        <p style="margin-block-start: 3px;">Freitag: 18:00 Uhr - bis fertig<br>Samstag: 12:00 Uhr - bis fertig</p>\
+        <p>Preis: CHF 200.-</p>\
+        Status: Frei'
     ],
 
     [
@@ -106,7 +86,11 @@ function initMap() {
         "images/bar.png",
         25, 
         25,
-        '<h2>Letzte Festung</h2><p><a href="">www.samisbest.com</a></p>'
+        '<h2>Letzte Festung</h2>\
+        <h3 style="margin-block-end: 3px;">Öffnungszeiten</h3>\
+        <p style="margin-block-start: 3px;">Freitag: 18:00 Uhr - bis fertig<br>Samstag: 12:00 Uhr - bis fertig</p>\
+        <p>Preis: CHF 300.-</p>\
+        Status: Frei'
     ],
 
     [
@@ -116,7 +100,11 @@ function initMap() {
         "images/bar.png",
         25, 
         25,
-        '<h2>Stall des Paten</h2><p><a href="">www.samisbest.com</a></p>'
+        '<h2>Stall des Paten</h2>\
+        <h3 style="margin-block-end: 3px;">Öffnungszeiten</h3>\
+        <p style="margin-block-start: 3px;">Freitag: 18:00 Uhr - bis fertig<br>Samstag: 18:00 Uhr - bis fertig</p>\
+        <p>Preis: CHF 200.-</p>\
+        Status: Reserviert'
     ],
 
     [
@@ -126,9 +114,72 @@ function initMap() {
         "images/bar.png",
         25, 
         25,
-        '<h2>LM LaserArt Eventtechnik</h2><p><a href="">www.samisbest.com</a></p>'
+        '<h2>LM LaserArt Eventtechnik</h2>\
+        <h3 style="margin-block-end: 3px;">Öffnungszeiten</h3>\
+        <p style="margin-block-start: 3px;">Freitag: 18:00 Uhr - bis fertig<br>Samstag: 18:00 Uhr - bis fertig</p>\
+        <p>Preis: CHF 200.-</p>\
+        Status: Frei'
     ],
   ]; 
+
+    // Bar-marker
+    const food_bars = [
+      [
+          "Parkplatz Maxenhaus",
+          46.311729,
+          7.800288,
+          "images/Gastro.png",
+          25, 
+          25,
+          '<h2>Parkplatz Maxenhaus</h2>\
+          <h3 style="margin-block-end: 3px;">Öffnungszeiten</h3>\
+          <p style="margin-block-start: 3px;">Freitag: 18:00 Uhr - bis fertig<br>Samstag: 11:00 Uhr - bis fertig</p>\
+          <p>Preis: CHF 200.-</p>\
+          Status: Frei'
+      ],
+  
+      [
+          "Burgersaal",
+          46.311560,
+          7.800238,
+          "images/Gastro.png",
+          25, 
+          25,
+          '<h2>Burgersaal</h2>\
+          <h3 style="margin-block-end: 3px;">Öffnungszeiten</h3>\
+          <p style="margin-block-start: 3px;">Samstag: 11:00 Uhr - 14:00 Uhr</p>\
+          <p>Preis: CHF 200.-</p>\
+          Status: Reserviert'
+      ],
+  
+      [
+          "Stall vis-a-vis Burgersaal",
+          46.311605,
+          7.800008,
+          "images/Gastro.png",
+          25, 
+          25,
+          '<h2>Stall vis-a-vis Burgersaal</h2>\
+          <h3 style="margin-block-end: 3px;">Öffnungszeiten</h3>\
+          <p style="margin-block-start: 3px;">Freitag: 18:00 Uhr - bis fertig<br>Samstag: 11:00 Uhr - bis fertig</p>\
+          <p>Preis: CHF 300.-</p>\
+          Status: Frei'
+      ],
+  
+      [
+          "Platz vor Jublagarage",
+          46.311493,
+          7.800209,
+          "images/Gastro.png",
+          25, 
+          25,
+          '<h2>Platz vor Jublagarage</h2>\
+          <h3 style="margin-block-end: 3px;">Öffnungszeiten</h3>\
+          <p style="margin-block-start: 3px;">Freitag: 18:00 Uhr - bis fertig<br>Samstag: 11:00 Uhr - bis fertig</p>\
+          <p>Preis: CHF 200.-</p>\
+          Status: Reserviert'
+      ],
+    ]; 
 
   // Restaurant-marker
   const restaurants = [
@@ -136,30 +187,30 @@ function initMap() {
         "Restaurant Schmitta",
         46.311236,
         7.799061,
-        "images/Gastro.png",
+        "images/Restaurant.png",
         25, 
         25,
-        '<h2>Restaurant Schmitta</h2><p><a href="">www.samisbest.com</a></p>'
+        '<h2>Restaurant Schmitta</h2><br><h3>Öffnungszeiten</h3><p>Freitag: 18:00 Uhr - bis fertig<br>Samstag: 12:00 Uhr - bis fertig</p>'
     ],
 
     [
         "Restaurant Rilke",
         46.310952,
         7.800120,
-        "images/Gastro.png",
+        "images/Restaurant.png",
         25, 
         25,
-        '<h2>Restaurant Rilke</h2><p><a href="">www.samisbest.com</a></p>'
+        '<h2>Restaurant Rilke</h2><br><h3>Öffnungszeiten</h3><p>Freitag: 18:00 Uhr - bis fertig<br>Samstag: 12:00 Uhr - bis fertig</p>'
     ],
 
     [
-        "Beck",
+        "Bäckerei Zenhäusern",
         46.310503,
         7.799832,
-        "images/Gastro.png",
+        "images/Restaurant.png",
         25, 
         25,
-        '<h2>Beck</h2><p><a href="">www.samisbest.com</a></p>'
+        '<h2>Bäckerei Zenhäusern</h2><br><h3>Öffnungszeiten</h3><p>Freitag: 18:00 Uhr - 22:00 Uhr<br>Samstag: 12:00 Uhr - 22:00 Uhr</p>'
     ],
   ];
 
@@ -237,7 +288,18 @@ function initMap() {
   });
 
   const stage_infowindow = new google.maps.InfoWindow({
-    content: "<h3>Festprogramm</h3><p>18:00 - 20:00: Sam41<p>",
+    content: 
+    '<h3>Festprogramm</h3>\
+    <h3 style="margin-block-end: 3px;">Freitag</h3>\
+    <p style="margin-block-start: 3px; margin-block-end: 3px;">19:00 Uhr - 20:00 Uhr: TripleH-B</p>\
+    <p style="margin-block-start: 3px; margin-block-end: 3px;">18:00 Uhr - 20:00 Uhr: Riverbanks</p>\
+    <p style="margin-block-start: 3px;">22:30 Uhr - 24:00 Uhr: The Unwritten Story</p>\
+    <h3 style="margin-block-end: 3px;">Samstag</h3>\
+    <p style="margin-block-start: 3px; margin-block-end: 3px;">14:00 Uhr - 16:00 Uhr: Bietschibotsche</p>\
+    <p style="margin-block-start: 3px; margin-block-end: 3px;">16:00 Uhr - 17:00 Uhr: Mini Playback Show by Jubla</p>\
+    <p style="margin-block-start: 3px; margin-block-end: 3px;">19:00 Uhr - 20:00 Uhr: Kentucky Moonshiners</p>\
+    <p style="margin-block-start: 3px; margin-block-end: 3px;">20:00 Uhr - 21:30 Uhr: Madstone</p>\
+    <p style="margin-block-start: 3px;">22:30 Uhr - 24:00 Uhr: TBA</p>',
   });
 
   stage.addListener("click", () => {
@@ -281,8 +343,42 @@ function initMap() {
 
 
   // Place markers on map
-  for (let i = 0; i < bars.length; i++){
-    const currMarker = bars[i]
+  for (let i = 0; i < drink_bars.length; i++){
+    const currMarker = drink_bars[i]
+
+    const marker = new google.maps.Marker({
+        position: { lat: currMarker[1], lng: currMarker[2] },
+        map: map,
+        title: currMarker[0],
+        icon: {
+            url: currMarker[3],
+            scaledSize: new google.maps.Size(currMarker[4], currMarker[5]),
+            optimized: false 
+        },
+      });
+    
+      const infowindow = new google.maps.InfoWindow({
+        content: currMarker[6],
+      });
+    
+      marker.addListener("click", () => {
+        infowindow.open({
+          anchor: marker,
+          map,
+        });
+      });
+
+      google.maps.event.addListener(map, "zoom_changed", function() {
+        if (map.getZoom() < 17) {
+            marker.setVisible(false);
+        } else {
+            marker.setVisible(true);
+        }
+      });
+  }
+
+  for (let i = 0; i < food_bars.length; i++){
+    const currMarker = food_bars[i]
 
     const marker = new google.maps.Marker({
         position: { lat: currMarker[1], lng: currMarker[2] },
